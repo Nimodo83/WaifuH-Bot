4d5524a200e3271d872e48aa1cf586e86ea52a8a
Cuervo-Team-Supreme <145875276+Nimodo83@users.noreply.github.com>
2024-11-22 16:52:20 -0600

    Add files via upload

---
+++ puglins/Config•welcome.js
@@ -0,0 +1,44 @@
+import {WAMessageStubType} from '@whiskeysockets/baileys'
+import fetch from 'node-fetch'
+
+export async function before(m, {conn, participants, groupMetadata}) {
+  if (!m.messageStubType || !m.isGroup) return !0;
+  let pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(_ => 'https://ibb.co/WBYqRDd')
+  let img = await (await fetch(`${pp}`)).buffer()
+  let chat = global.db.data.chats[m.chat]
+  
+  let who = m.messageStubParameters[0] + '@s.whatsapp.net';
+  let user = global.db.data.users[who];
+
+  let userName = user ? user.name : await conn.getName(who);
+
+  if (chat.welcome && m.messageStubType == 27) {
+    let bienvenida = `*⭒─ׄ─ׅ─ׄ─⭒ Welcome User ⭒─ׄ─ׅ─ׄ─⭒*
+    
+╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*
+┊ ‹‹ *Welcome* : *@${m.messageStubParameters[0].split`@`[0]}⁩*
+┊•*⁀➷ °⭒⭒⭒ *【 ✯ ${groupMetadata.subject} ✰ 】*
+╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩`;
+    
+await conn.sendMini(m.chat, packname, textbot, bienvenida, img, img, channel, estilo)
+  }
+  
+  if (chat.welcome && m.messageStubType == 32) {
+    let bye = `*⭒─ׄ─ׅ─ׄ─⭒ Bye User ⭒─ׄ─ׅ─ׄ─⭒*
+    
+╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*
+┊ ‹‹ *Bye* : *@${m.messageStubParameters[0].split`@`[0]}⁩*
+┊•*⁀➷ °⭒⭒⭒ *【 ✯ ${groupMetadata.subject} ✰ 】*
+╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩`;
+await conn.sendMini(m.chat, packname, textbot, bye, img, img, channel, estilo)
+  }
+  
+  if (chat.welcome && m.messageStubType == 28) {
+    let kick = `*⭒─ׄ─ׅ─ׄ─⭒ Adios Put@ ⭒─ׄ─ׅ─ׄ─⭒*
+    
+╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*
+┊ ‹‹ *Pendej@ fuera* : *@${m.messageStubParameters[0].split`@`[0]}⁩*
+┊•*⁀➷ °⭒⭒⭒ *【 ✯ ${groupMetadata.subject} ✰ 】*
+╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩`;
+await conn.sendMini(m.chat, packname, textbot, kick, img, img, channel, estilo)
+}}
