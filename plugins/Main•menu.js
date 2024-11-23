713816c388b7b1866231a816a8a60a6c2cf5c26f
DevDiego <151096440+Dev-Diego@users.noreply.github.com>
2024-11-23 09:39:08 -0500

    Actualizar Main•menu.js

--- plugins/Main•menu.js
+++ plugins/Main•menu.js
@@ -7,6 +7,7 @@ import { promises } from 'fs'
 import { join } from 'path'
 let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text, command }) => {
 try {        
+let redes = ['global.channel', 'global.grupo', 'global.git'].getRandom()
 let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
 let { exp, monedas, level, role } = global.db.data.users[m.sender]
 let { min, xp, max } = xpRange(level, global.multiplier)
